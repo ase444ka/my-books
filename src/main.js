1c7e45a4fb01dd7d2f135f24ea1e1f06cb84625a
Ase444ka <istomina.asia@yandex.ru>
2025-01-18 21:28:59 +0300

    remove router and pinia

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import './assets/main.scss';
 
 import {createApp} from 'vue';
-import {createPinia} from 'pinia';
 
 import MyCheckbox from '@/components/ui/MyCheckbox.vue';
 import MyInput from '@/components/ui/MyInput.vue';
 import MyButton from '@/components/ui/MyButton.vue';
 
 import App from './App.vue';
-import router from './router';
 
 import noty from '@/plugins/noty.js'
 
@@ -19,8 +17,6 @@ app
   .component('MyInput', MyInput)
   .component('MyButton', MyButton)
 
-app.use(createPinia());
-app.use(router);
 app.use(noty);
 app.mount('#app');
 
